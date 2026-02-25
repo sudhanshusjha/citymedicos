0339d86b1a161646bf50c7238ac5439d04c142ca
sudhanshusjha <sudhanshurg@gmail.com>
2026-02-25 13:30:14 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
